a8e1857e91871ebef44686a5cb0c50ae56e67c73
Alex <pioneerko@hotmail.com>
2021-11-17 20:49:08 +0000

    Created nav header

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,10 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+                 ]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
--- home/views.py
+++ home/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world")
+    return render(request, "home/index.html")
