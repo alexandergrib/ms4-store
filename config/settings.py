4ec399f780ae67c00e3d4b05e91e9e90caf807da
Alex <pioneerko@hotmail.com>
2021-11-25 20:35:10 +0000

    Add postgress database

--- config/settings.py
+++ config/settings.py
@@ -66,8 +66,7 @@ TEMPLATES = [
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
             os.path.join(BASE_DIR, 'templates', 'allauth'),
-                 ]
-        ,
+                 ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
