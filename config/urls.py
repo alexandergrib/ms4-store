143786797ce6a445a2ac02ae24987f2cf883a87b
Alex <pioneerko@hotmail.com>
2021-12-01 20:03:08 +0000

    Added categories search

--- config/urls.py
+++ config/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('tinymce/', include('tinymce.urls')),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
