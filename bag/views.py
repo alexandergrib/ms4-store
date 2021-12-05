7fdd777a30335042138b3f926a29317c53fad55c
Alex <pioneerko@hotmail.com>
2021-12-05 16:16:19 +0000

    addded custom template filter to update total quantity in the basket

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, reverse
 
 
 def view_bag(request):
@@ -22,3 +23,32 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     print(request.session['bag'])
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Adjust a quantity of the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """ Remove an item from the shopping bag """
+    try:
+        bag = request.session.get('bag', {})
+        bag.pop(item_id)
+
+        request.session['bag'] = bag
+        print(request.session['bag'])
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
