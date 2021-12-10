0ea483b21fa85a290592505f8000183971e782cd
Alex <pioneerko@hotmail.com>
2021-12-10 10:04:27 +0000

    CODE: add toasts

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect, reverse
-
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from products.models import Product,Cartridges
 
 def view_bag(request):
     """ A view that renders the bag contents page """
@@ -10,15 +11,20 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
-
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+    except Http404:  # if not product then it's a cartridge
+        product = get_object_or_404(Cartridges, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Added {product.name} to your bag')
     else:
         bag[item_id] = quantity
+        messages.error(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     print(request.session['bag'])
--- static/js/main.js
+++ static/js/main.js
@@ -6,4 +6,3 @@ function handleChange() {
 }
 handleChange()
 
-
