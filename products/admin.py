36f159f4b551ad8a46204e7b2f54fcdb5da3f0d6
Alex <pioneerko@hotmail.com>
2021-12-10 14:52:37 +0000

    CODE: create checkout app, models, admin setting

--- products/admin.py
+++ products/admin.py
@@ -17,7 +17,22 @@ list_display = (
 )
 
 
+class ImagesAdminInline(admin.TabularInline):
+    model = ProductImages
+
+
+class SpecsAdminInline(admin.TabularInline):
+    model = ProductSpecifications
+
+
+class CartridgesAdminInline(admin.TabularInline):
+    verbose_name = "Cartridges"
+    verbose_name_plural = "Cartridges"
+    model = Cartridges.compatible_printer.through
+
+
 class ProductAdmin(admin.ModelAdmin):
+    inlines = (ImagesAdminInline, SpecsAdminInline, CartridgesAdminInline)
     ordering = ('-id',)
 
 
@@ -26,6 +41,8 @@ class CategoryAdmin(admin.ModelAdmin):
         'friendly_name',
         'name',
     )
+
+
 
 
 class ProductImagesAdmin(admin.ModelAdmin):
@@ -61,10 +78,15 @@ class CartridgesAdmin(admin.ModelAdmin):
     ordering = ('brand',)
 
 
+
+
+
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(ProductReviews, ProductReviewsAdmin)
-admin.site.register(ProductImages, ProductImagesAdmin)
-admin.site.register(ProductSpecifications, ProductSpecificationsAdmin)
+# admin.site.register(ProductImages, ProductImagesAdmin)
+# admin.site.register(ProductSpecifications, ProductSpecificationsAdmin)
 admin.site.register(ProductBrand, ProductBrandAdmin)
 admin.site.register(Cartridges, CartridgesAdmin)
