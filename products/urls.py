2f66b1175626becf5d18de99ae1f51c743f1a8b6
Alex <pioneerko@hotmail.com>
2021-12-12 21:01:27 +0000

    Add product form

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail'),
+    path('<uuid:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
+    path('edit/<uuid:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<uuid:product_id>/', views.delete_product,
+         name='delete_product'),
 ]
