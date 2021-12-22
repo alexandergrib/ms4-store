4e93abe0bd9978faf931c99b1eeb9235bfc5ffe8
alexandergrib <pioneerko@hotmail.com>
2021-12-22 14:41:49 +0000

    Category test

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import (Category, Product)
 
-# Create your tests here.
+
+class CategoryTestCase(TestCase):
+    def setUp(self):
+        Category.objects.create(name="laser", friendly_name="Laser")
+
+
+    def test_category(self):
+        """Test Category is created succesfuly"""
+        category = Category.objects.get(name="laser")
+        self.assertEqual(category.get_friendly_name(), 'Laser')
+        
