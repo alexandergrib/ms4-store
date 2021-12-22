8d7d6f5124431f9fde91ece11665a346025180c8
Alex <pioneerko@hotmail.com>
2021-12-22 19:52:55 +0000

    Product tests WIP

--- products/tests.py
+++ products/tests.py
@@ -1,14 +1,27 @@
 from django.test import TestCase
-from .models import (Category, Product)
+from .models import (Category, Product, ProductBrand)
+import datetime
 
 
 class CategoryTestCase(TestCase):
-    def setUp(self):
-        Category.objects.create(name="laser", friendly_name="Laser")
+    # def setUp(self):
+    #     category = Category.objects.get(pk=1)
+    #     brand = ProductBrand.objects.get(pk=1)
+    #     product = Product.objects.get(pk='9765521f-1f87-4a75-a894-e50e539841cc')
 
+    def test_product_name(self):
+        """
+        Test that the product name is retrieved correctly
+        """
+        product = Product.objects.get(pk='9765521f-1f87-4a75-a894-e50e539841cc')
+        self.assertEqual(product.name, 'A4 Mono Laser Printer')
 
     def test_category(self):
-        """Test Category is created succesfuly"""
-        category = Category.objects.get(name="laser")
-        self.assertEqual(category.get_friendly_name(), 'Laser')
-        
+        """Test Category is created successfully"""
+        category = Category.objects.get(pk=1)
+        self.assertEqual(category.get_friendly_name(), 'Laser printers')
+
+    def test_brand(self):
+        """Test Brand is created successfully"""
+        brand = ProductBrand.objects.get(pk=1)
+        self.assertEqual(brand.get_friendly_name(), 'Brother')
