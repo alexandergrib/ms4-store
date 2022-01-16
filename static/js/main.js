39589cfa20fa1ecbcf64dee02638b2902d3af979
Alex <pioneerko@hotmail.com>
2022-01-16 12:14:06 +0000

    Adjusted positioning of images on the featured products page, removed description from featured

--- static/js/main.js
+++ static/js/main.js
@@ -4,5 +4,6 @@ function handleChange() {
     let headerSpacerHeight = document.getElementsByTagName("header")[0].offsetHeight;
     document.getElementById("header-spacer").style.height = (headerSpacerHeight + "px");
 }
-handleChange()
 
+
+$(document).ready(handleChange)
