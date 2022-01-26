b6913a8920c71d6a5b705f3a8300be35386f8e9b
Alex <pioneerko@hotmail.com>
2022-01-26 14:40:16 +0000

    checkout app tests

---
+++ checkout/test_models.py
@@ -0,0 +1,82 @@
+# Most of the testing code was used from https://github.com/Abibubble/ms4-lead-shot-hazard/ with adaptation to suit my project
+from django.test import TestCase
+from django.conf import settings
+
+from products.models import Product
+from .models import Order
+
+
+class TestOrderModels(TestCase):  # pragma: no cover
+    """
+    Test that the order models work as expected
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        Order.objects.create(
+            full_name='tester test',
+            email='test@example.com',
+            phone_number='09876543212',
+            country='GB',
+            town_or_city='test',
+            street_address1='test'
+        )
+
+    def test_order_string_method_returns_order_number(self):
+        """
+        Test string method for Order class
+        """
+        order_number = Order.objects.create(order_number='2000')
+        self.assertEqual(str(order_number), '2000')
+
+    def test_checkout_details(self):
+        """
+        Test that the checkout fields auto-fill from the user's information
+        """
+        order = Order.objects.get(id=1)
+        self.assertEqual(order.full_name, 'tester test')
+        self.assertEqual(order.email, 'test@example.com')
+        self.assertEqual(order.phone_number, '09876543212')
+        self.assertEqual(order.country, 'GB')
+        self.assertEqual(order.street_address1, 'test')
+
+    def test_update_total(self):
+        """
+        Test that the order total is updated correctly
+        """
+        sdp = settings.STANDARD_DELIVERY_PERCENTAGE
+        order_total = 10
+        if order_total < settings.FREE_DELIVERY_THRESHOLD:
+            delivery_cost = order_total * sdp / 100
+            self.assertEqual(delivery_cost, 1)
+        else:
+            delivery_cost = 0
+            self.assertEqual(delivery_cost, 0)
+        grand_total = order_total + delivery_cost
+        self.assertEqual(grand_total, 11)
+        order_total = 50
+        if order_total < settings.FREE_DELIVERY_THRESHOLD:
+            delivery_cost = order_total * sdp / 100
+            self.assertEqual(delivery_cost, 1)
+        else:
+            delivery_cost = 0
+            self.assertEqual(delivery_cost, 0)
+        grand_total = order_total + delivery_cost
+        self.assertEqual(grand_total, 50)
+
+
+class TestOrderLineItemModels(TestCase):
+    """
+    Test that the order models work as expected
+    """
+
+    def test_order_line_item_string_method(self):
+        """
+        Test that the string method works for the OrderLineItem class
+        """
+        product = Product.objects.create(sku='pp200', price=2.99, discount=0)
+        order = Order.objects.create(order_number='2000')
+        expected_output = 'SKU pp200 on order 2000'
+        self.assertEqual(str(
+            f'SKU {product.sku} on order {order.order_number}'),
+            expected_output)
---
+++ checkout/test_views.py
@@ -0,0 +1,151 @@
+# Most of the testing code was used from https://github.com/Abibubble/ms4-lead-shot-hazard/ with adaptation to suit my project
+from django.test import TestCase
+
+from django.shortcuts import reverse, get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib.messages import get_messages
+from django.conf import settings
+
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
+from products.models import Product, Category
+
+
+class TestCheckoutViews(TestCase):
+    """
+    Test that the checkout page and checkout cache work as expected
+    """
+
+    fixtures = [
+        'fixtures.json'
+    ]
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='test@test.com',
+            password='te12345st'
+        )
+
+        self.admin = User.objects.create_superuser(
+            username='testadmin',
+            email='testadmin@test.com',
+            password='te12345st'
+        )
+
+        self.category = Category.objects.create(
+            name='testcategory',
+            friendly_name='Test Category'
+        )
+
+        self.item = Product.objects.create(
+            category=self.category,
+            name='test product',
+            description='testing product description',
+            price=0.01,
+            discount=0,
+            sku='1111',
+        )
+
+        self.quantity = 1
+
+        self.empty_bag = {}
+
+        self.filled_bag = {'1b6622c4-3fde-4afe-b3bd-673a038454b6': 1, '8f6656a5-67c3-498b-ab10-b87b66564a79': 1}
+
+        self.bad_bag = {'200': 1}
+
+    def test_cache_checkout_data(self):
+        """
+        Test that the cache_checkout_data function responds correctly
+        """
+        response = self.client.get('/checkout/cache_checkout_data/')
+        self.assertEqual(response.status_code, 405)
+
+    def test_the_checkout_page_url_exists(self):
+        """
+        Test that the checkout page URL exists
+        """
+        response = self.client.get('/checkout/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_the_checkout_url_is_accessible_by_name(self):
+        """
+        Test that the checkout page is accessible via name
+        """
+        response = self.client.get(reverse('checkout'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_checkout_view_uses_correct_template(self):
+        """
+        Test that the checkout page redirects correctly
+        """
+        response = self.client.get(reverse('checkout'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/products/')
+
+    def test_nothing_in_bag_error(self):
+        """
+        Test that an error message is shown when nothing is in the shopping bag
+        """
+        response = self.client.get('/checkout/')
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(messages[0].tags, 'error')
+        self.assertEqual(
+            str(messages[0]), "There's nothing in your bag at the moment")
+
+    def test_no_stripe_key_error(self):
+        """
+        Test that an error message is shown when Stripe key is missing
+        """
+        bag = {'8f6656a5-67c3-498b-ab10-b87b66564a79': 1, '1b6622c4-3fde-4afe-b3bd-673a038454b6': 1}
+        session = self.client.session
+        session['bag'] = bag
+        session.save()
+        settings.STRIPE_PUBLIC_KEY = ''
+        response = self.client.get('/checkout/')
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(messages[0].tags, 'warning')
+        self.assertEqual(
+            str(messages[0]), 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
+    def test_get_checkout_view(self):
+        """
+        Test that the checkout view works with items in the shopping bag
+        """
+        session = self.client.session
+        session['bag'] = self.filled_bag
+        session.save()
+        response = self.client.get('/checkout/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_checkout_autofill_details_if_logged_in(self):
+        """
+        Check if user is authenticated and logged in,
+        and if they are, autofill the form with their details
+        """
+        session = self.client.session
+        session['bag'] = self.filled_bag
+        session.save()
+        self.client.force_login(self.user)
+        response = self.client.get('/checkout/')
+        user_profile = get_object_or_404(UserProfile, user=self.user)
+        user_profile_form = UserProfileForm({
+            'default_phone_number': '09876543212',
+            'default_street_address11': 'test',
+            'default_street_address2': 'test',
+            'default_town_or_city': 'test',
+            'default_county': 'test',
+            'default_postcode': 'test',
+            'default_country': 'GB'
+        },
+            instance=user_profile
+        )
+
+        self.assertTrue(user_profile_form.is_valid())
+        user_profile_form.save()
+        order_form = response.context['order_form']
+        self.assertGreater(len(order_form.initial), 1)
