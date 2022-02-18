0ee5cce723db0890640405e9af62fb81c953f866
Alex <pioneerko@hotmail.com>
2022-02-18 15:13:40 +0000

    doc: readme updated

--- bag/contexts.py
+++ bag/contexts.py
@@ -5,6 +5,9 @@ from django.shortcuts import get_object_or_404
 
 from products.models import ProductBrand, Category, Product, Cartridges
 
+delivery = 0
+free_delivery_delta = 0
+
 
 def bag_contents(request):
     bag_items = []
@@ -40,9 +43,8 @@ def bag_contents(request):
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
-    else:
-        delivery = 0
-        free_delivery_delta = 0
+
+
 
     grand_total = delivery + total
 
