3062c8c65bf7f1e475caf4f1bb0ccd513f603554
Alex <pioneerko@hotmail.com>
2023-01-24 22:07:48 +0000

    sdasdasd

--- config/urls.py
+++ config/urls.py
@@ -14,5 +14,3 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('tinymce/', include('tinymce.urls')),
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
